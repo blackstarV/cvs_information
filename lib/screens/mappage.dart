69c0a1bca0422fd2726c73808b0b78a50f5b0580
blackstarV <114997245+blackstarV@users.noreply.github.com>
2022-10-11 04:14:18 +0900

    맵페이지 수정
    1. Sliding_Up_Panel 사용해서 편의점 목록 구현 완료
    2. 현재위치 버튼 누르면 현재위치로 카메라 이동 구현 완료
    3. 각 리스트타일 클릭하면 각 편의점 위치로 이동 구현 완료
    
    맵페이지 요구사항 :
    편의점 커스텀 마커 구현 예정

--- lib/screens/mappage.dart
+++ lib/screens/mappage.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io' show Platform;
 
 import 'package:cvs_information/services/geolocator_service.dart';
@@ -10,6 +11,7 @@ import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../models/place.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -19,19 +21,20 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
-  /* 카메라 포지션 기본값 공주대학교
-  static const double _defaultLat = 36.8511811;
-  static const double _defaultLng = 127.1511352;
+  static const double gpsButtonHeightClosed = 120;
+  double gpsButtonHeight = gpsButtonHeightClosed;
+  final panelController = PanelController();
+  Completer<GoogleMapController> mapcontroller = Completer();
 
-  static const CameraPosition _defaultLocation =
-      CameraPosition(target: LatLng(_defaultLat, _defaultLng), zoom: 15);
-  */
   @override
   Widget build(BuildContext context) {
     final currentPosition = Provider.of<Position?>(context);
     final placesProvider = Provider.of<Future<List<Place>?>?>(context);
     final geoService = GeoLocatorService();
     final markerService = MarkerService();
+    final panelHeightOpen = MediaQuery.of(context).size.height * 0.6;
+    final panelHeightClosed = MediaQuery.of(context).size.height * 0.1;
+    final panelMaxScrollExtent = panelHeightOpen - panelHeightClosed;
 
     return FutureProvider(
       initialData: null,
@@ -43,125 +46,172 @@ class _MapPageState extends State<MapPage> {
                   var markers = (places != null)
                       ? markerService.getMarkers(places)
                       : <Marker>[];
-                  return Column(
-                    children: <Widget>[
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height / 3,
+                  return Stack(alignment: Alignment.topCenter, children: <
+                      Widget>[
+                    SlidingUpPanel(
+                      controller: panelController,
+                      maxHeight: panelHeightOpen,
+                      minHeight: panelHeightClosed,
+                      parallaxEnabled: true,
+                      parallaxOffset: 0.5,
+                      borderRadius:
+                          const BorderRadius.vertical(top: Radius.circular(30)),
+                      onPanelSlide: (position) => setState(() {
+                        gpsButtonHeight = position * panelMaxScrollExtent +
+                            gpsButtonHeightClosed;
+                      }),
+                      body: SizedBox(
+                        height: MediaQuery.of(context).size.height,
                         width: MediaQuery.of(context).size.width,
                         child: GoogleMap(
+                          myLocationEnabled: true,
                           initialCameraPosition: CameraPosition(
                               target: LatLng(currentPosition.latitude,
                                   currentPosition.longitude),
                               zoom: 16.0),
                           zoomGesturesEnabled: true,
                           markers: Set<Marker>.of(markers),
+                          onMapCreated: (GoogleMapController controller) {
+                            mapcontroller.complete(controller);
+                          },
                         ),
                       ),
-                      const SizedBox(
-                        height: 10.0,
-                      ),
-                      Expanded(
-                        child: ListView.builder(
-                            itemCount: places?.length,
-                            itemBuilder: (context, index) {
-                              return (places != null)
-                                  ? FutureProvider(
-                                      create: (context) =>
-                                          geoService.getDistance(
-                                              currentPosition.latitude,
-                                              currentPosition.longitude,
-                                              places[index]
-                                                  .geometry
-                                                  .location
-                                                  .lat,
-                                              places[index]
-                                                  .geometry
-                                                  .location
-                                                  .lng),
-                                      initialData: null,
-                                      child: Card(
-                                        child: ListTile(
-                                          onTap: () {},
-                                          //----------------------------------------------------------------------
-                                          title: Text(places[index].name),
-                                          subtitle: Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: [
-                                              //----------------------------------------------------------------------
-                                              const SizedBox(
-                                                height: 5.0,
-                                              ),
-                                              //----------------------------------------------------------------------
-                                              (places[index].rating !=
-                                                      0.0) // rating이 있을 땐 표시, 없으면 표시 안함
-                                                  ? Row(
-                                                      children: [
-                                                        RatingBarIndicator(
-                                                          rating: places[index]
-                                                              .rating,
-                                                          itemBuilder: (context,
-                                                                  index) =>
-                                                              const Icon(
-                                                                  Icons.star),
-                                                          itemCount: 5,
-                                                          itemSize: 10.0,
-                                                          direction:
-                                                              Axis.horizontal,
-                                                        )
-                                                      ],
-                                                    )
-                                                  : Row(),
-                                              //----------------------------------------------------------------------
-                                              const SizedBox(
-                                                height: 5.0,
-                                              ),
-                                              //----------------------------------------------------------------------
-                                              Consumer<double?>(
-                                                builder:
-                                                    (context, meters, widget) {
-                                                  return (meters != null)
-                                                      ? Column(
-                                                          crossAxisAlignment:
-                                                              CrossAxisAlignment
-                                                                  .start,
-                                                          children: [
-                                                            Text(places[index]
-                                                                .vicinity),
-                                                            (meters >= 1000)
-                                                                ? Text(
-                                                                    '${(meters / 1000).toStringAsFixed(1)} km')
-                                                                : Text(
-                                                                    '${meters.toInt()} m'),
-                                                          ],
-                                                        )
-                                                      : Container();
-                                                },
-                                              )
-                                            ],
-                                          ),
-                                          //----------------------------------------------------------------------
-                                          trailing: IconButton(
-                                            icon:
-                                                const Icon(Icons.info_outline),
-                                            onPressed: () {
-                                              _launchMapsUrl(
-                                                  places[index].place_id);
+                      panelBuilder: (ScrollController controller) =>
+                          ListView.builder(
+                              padding: const EdgeInsets.symmetric(vertical: 30),
+                              controller: controller,
+                              itemCount: places?.length,
+                              itemBuilder: (context, index) {
+                                return (places != null)
+                                    ? FutureProvider(
+                                        create: (context) =>
+                                            geoService.getDistance(
+                                                currentPosition.latitude,
+                                                currentPosition.longitude,
+                                                places[index]
+                                                    .geometry
+                                                    .location
+                                                    .lat,
+                                                places[index]
+                                                    .geometry
+                                                    .location
+                                                    .lng),
+                                        initialData: null,
+                                        child: Card(
+                                          child: ListTile(
+                                            onTap: () async {
+                                              GoogleMapController controller =
+                                                  await mapcontroller.future;
+                                              controller.animateCamera(
+                                                  CameraUpdate
+                                                      .newCameraPosition(
+                                                          CameraPosition(
+                                                              target: LatLng(
+                                                                  places[index]
+                                                                      .geometry
+                                                                      .location
+                                                                      .lat,
+                                                                  places[index]
+                                                                      .geometry
+                                                                      .location
+                                                                      .lng),
+                                                              zoom: 16.0)));
                                             },
+                                            //----------------------------------------------------------------------
+                                            title: Text(places[index].name),
+                                            subtitle: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                //----------------------------------------------------------------------
+                                                const SizedBox(
+                                                  height: 5.0,
+                                                ),
+                                                //----------------------------------------------------------------------
+                                                (places[index].rating !=
+                                                        0.0) // rating이 있을 땐 표시, 없으면 표시 안함
+                                                    ? Row(
+                                                        children: [
+                                                          RatingBarIndicator(
+                                                            rating:
+                                                                places[index]
+                                                                    .rating,
+                                                            itemBuilder: (context,
+                                                                    index) =>
+                                                                const Icon(
+                                                                    Icons.star),
+                                                            itemCount: 5,
+                                                            itemSize: 10.0,
+                                                            direction:
+                                                                Axis.horizontal,
+                                                          )
+                                                        ],
+                                                      )
+                                                    : Row(),
+                                                //----------------------------------------------------------------------
+                                                const SizedBox(
+                                                  height: 5.0,
+                                                ),
+                                                //----------------------------------------------------------------------
+                                                Consumer<double?>(
+                                                  builder: (context, meters,
+                                                      widget) {
+                                                    return (meters != null)
+                                                        ? Column(
+                                                            crossAxisAlignment:
+                                                                CrossAxisAlignment
+                                                                    .start,
+                                                            children: [
+                                                              Text(places[index]
+                                                                  .vicinity),
+                                                              (meters >= 1000)
+                                                                  ? Text(
+                                                                      '${(meters / 1000).toStringAsFixed(1)} km')
+                                                                  : Text(
+                                                                      '${meters.toInt()} m'),
+                                                            ],
+                                                          )
+                                                        : Container();
+                                                  },
+                                                )
+                                              ],
+                                            ),
+                                            //----------------------------------------------------------------------
+                                            trailing: IconButton(
+                                              icon: const Icon(
+                                                  Icons.info_outline),
+                                              onPressed: () {
+                                                _launchMapsUrl(
+                                                    places[index].place_id);
+                                              },
+                                            ),
                                           ),
                                         ),
-                                      ),
-                                    )
-                                  : Platform.isAndroid
-                                      ? const Center(
-                                          child: CircularProgressIndicator())
-                                      : const Center(
-                                          child:
-                                              CupertinoActivityIndicator()); // 로딩창
-                            }),
-                      )
-                    ],
-                  );
+                                      )
+                                    : Platform.isAndroid
+                                        ? const Center(
+                                            child: CircularProgressIndicator())
+                                        : const Center(
+                                            child:
+                                                CupertinoActivityIndicator()); // 로딩창
+                              }),
+                    ),
+                    Positioned(
+                        right: 20,
+                        bottom: gpsButtonHeight,
+                        child: FloatingActionButton(
+                          child: const Icon(Icons.gps_fixed),
+                          onPressed: () async {
+                            GoogleMapController controller =
+                                await mapcontroller.future;
+                            controller.animateCamera(
+                                CameraUpdate.newCameraPosition(CameraPosition(
+                                    target: LatLng(currentPosition.latitude,
+                                        currentPosition.longitude),
+                                    zoom: 16.0)));
+                          },
+                        )),
+                  ]);
                 },
               )
             : Platform.isAndroid
