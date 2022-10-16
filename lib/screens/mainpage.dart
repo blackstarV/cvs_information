54b0ae3392c40688c2d860314fb5bfaa8a2fe2a8
blackstarV <114997245+blackstarV@users.noreply.github.com>
2022-10-17 00:50:36 +0900

    스크롤 시 네비게이션바 숨김추가
    Grid랑 연동안되서 개 고생했는데
    Sliver기능에 SliverGrid 라는게 있어서 연동하여 해결

--- lib/screens/mainpage.dart
+++ lib/screens/mainpage.dart
@@ -1,6 +1,7 @@
-import 'package:cvs_information/Screens/homepage.dart';
 import 'package:cvs_information/screens/mappage.dart';
+import 'package:cvs_information/widgets/flexiblespace.dart';
 import 'package:cvs_information/widgets/navigation_drawer.dart';
+import 'package:cvs_information/widgets/scroll_hide.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -10,26 +11,78 @@ class MainPage extends StatefulWidget {
   State<MainPage> createState() => _MainPageState();
 }
 
+List<String> items = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+];
+final ScrollController scrollController = ScrollController();
+
+@override
 class _MainPageState extends State<MainPage> {
   int index = 0;
+
   final screens = [
-    const HomePage(),
+    NestedScrollView(
+      scrollDirection: Axis.vertical,
+      controller: scrollController,
+      floatHeaderSlivers: true,
+      headerSliverBuilder: (context, innerBoxIsScrolled) {
+        return [
+          const SliverAppBar(
+            collapsedHeight: 302, // 접힌 높이
+            expandedHeight: 302, // 펴진 높이
+            floating: true,
+            snap: true,
+            centerTitle: true,
+            flexibleSpace: FlexibleSpace(),
+          )
+        ];
+      },
+      body: CustomScrollView(slivers: [
+        SliverGrid(
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 250,
+              mainAxisSpacing: 10,
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return Container(
+                  decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 224, 146, 146)),
+                  child: GridTile(
+                    child: Center(child: Text(items[index])),
+                  ),
+                );
+              },
+              childCount: items.length,
+            )),
+      ]),
+    ),
     const MapPage(),
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: screens[index],
-      drawer: const NavigationDrawer(),
-      bottomNavigationBar: NavigationBar(
-        height: 55,
-        selectedIndex: index,
-        onDestinationSelected: (index) => setState(() => this.index = index),
-        destinations: const [
-          NavigationDestination(icon: Icon(Icons.home), label: '홈'),
-          NavigationDestination(icon: Icon(Icons.map), label: '지도'),
-        ],
-      ),
-    );
+        body: screens[index],
+        drawer: const NavigationDrawer(),
+        bottomNavigationBar: ScrollToHideWidget(
+            scrollController: scrollController,
+            child: NavigationBar(
+              height: 55,
+              selectedIndex: index,
+              onDestinationSelected: (index) =>
+                  setState(() => this.index = index),
+              destinations: const [
+                NavigationDestination(icon: Icon(Icons.home), label: '홈'),
+                NavigationDestination(icon: Icon(Icons.map), label: '지도'),
+              ],
+            )));
   }
 }
---
+++ lib/widgets/scroll_hide.dart
@@ -0,0 +1,72 @@
+import 'package:cvs_information/screens/mainpage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class ScrollToHideWidget extends StatefulWidget {
+  final Widget child;
+  final ScrollController scrollController;
+  final Duration duration;
+
+  const ScrollToHideWidget(
+      {Key? key,
+      required this.child,
+      required this.scrollController,
+      this.duration = const Duration(milliseconds: 200)})
+      : super(key: key);
+
+  @override
+  State<ScrollToHideWidget> createState() => _ScrollToHideWidgetState();
+}
+
+class _ScrollToHideWidgetState extends State<ScrollToHideWidget> {
+  bool isVisible = true;
+  @override
+  void initState() {
+    super.initState();
+
+    widget.scrollController.addListener(listen);
+  }
+
+  @override
+  void dispose() {
+    widget.scrollController.addListener(listen);
+
+    super.dispose();
+  }
+
+  /*void listen() {
+    final direction = widget.scrollController.position.userScrollDirection;
+
+    if (direction == ScrollDirection.forward) {
+      show();
+    } else if (direction == ScrollDirection.reverse) {
+      hide();
+    }
+  }*/
+  void listen() {
+    if (widget.scrollController.positions.any(
+        (element) => element.userScrollDirection == ScrollDirection.forward)) {
+      show();
+    } else if (widget.scrollController.positions.any(
+        (element) => element.userScrollDirection == ScrollDirection.reverse)) {
+      hide();
+    }
+  }
+
+  void show() {
+    if (!isVisible) setState((() => isVisible = true));
+  }
+
+  void hide() {
+    if (isVisible) setState((() => isVisible = false));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: widget.duration,
+      height: isVisible ? kBottomNavigationBarHeight : 0,
+      child: Wrap(children: [widget.child]),
+    );
+  }
+}
